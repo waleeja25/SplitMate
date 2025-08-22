16979a99e9526c63600b656d96ddfef78070059d
Waleeja Ali <waleeja.ali25@gmail.com>
2025-08-22 17:38:32 +0500

    check

--- Backend/api/ip.js
+++ Backend/api/ip.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 export default async function handler(req, res) {
   const response = await fetch("https://api64.ipify.org?format=json");
   const data = await response.json();
-
+ console.log("Vercel outbound IP:", data.ip); 
   res.status(200).json({
     vercelOutboundIP: data.ip,
   });
